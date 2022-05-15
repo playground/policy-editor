1b04091e529a1481f9c210d376e63c217d198651
Jeff Lu <ljeff@us.ibm.com>
2022-05-15 13:40:10 -0400

    add interfaces

--- src/app/services/ieam.service.ts
+++ src/app/services/ieam.service.ts
@@ -3,6 +3,7 @@ import { HttpClient } from '@angular/common/http';
 import { ActivatedRoute } from '@angular/router';
 import { Observable } from 'rxjs';
 import { Params } from '../interface/params';
+import { ISession } from '../interface/session';
 
 export interface Broadcast {
   type: string;
@@ -13,6 +14,9 @@ export interface Broadcast {
 })
 export class IeamService {
   @Output() broadcastAgent = new EventEmitter<Broadcast>();
+  selectedRow!: { directory: any; type?: any; } | null;
+  fileType!: { enum: { DIRECTORY: any; }; };
+  offline = false;
 
   constructor(
     private http: HttpClient
@@ -24,17 +28,41 @@ export class IeamService {
   }
 
   get(url) {
-    return new Promise(async (resolve, reject) => {
-      try {
-        const response = await fetch(url);
-        resolve(await response.json());
-      } catch (e) {
-        resolve(null);
-      }
-    });
+    return this.http.get<Params>(url);
+    // return new Promise(async (resolve, reject) => {
+    //   try {
+    //     const response = await fetch(url);
+    //     resolve(await response.json());
+    //   } catch (e) {
+    //     resolve(null);
+    //   }
+    // });
   }
 
   post(url: string, data: any) {
     return this.http.post<Params>(url, data);
   }
+
+  getDirectory(f: string) {
+    const match = f.match(/.*\/([^/]*)\/[^/]*/);
+    return match ? match[1] : f.replace('/', '');
+  }
+
+  getFilename(f: string) {
+    const match = f.match(/\/([^\/]+)\/?$/);
+    return match ? match[1] : f;
+  }
+
+  setSession(key: string, value: string) {
+    sessionStorage.setItem(key, value);
+  }
+
+  getSession(key: string): string | null {
+    return sessionStorage.getItem(key);
+  }
+
+  removeSession(key: string) {
+    sessionStorage.removeItem(key);
+  }
+
 }
